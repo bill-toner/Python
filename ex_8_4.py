13532a4c4953199bc1d032005f3f63ed78fd4c16
Bill Toner <toner.bill@gmail.com>
2020-06-14 20:01:03 -0400

    added ex_8_4.py

---
+++ ex_8_4.py
@@ -0,0 +1,20 @@
+#  8.4 Open the file romeo.txt and read it line by line. 
+# For each line, split the line into a list of words using the split() method. 
+# The program should build a list of words. 
+# For each word on each line check to see if the word is already in the list and if not append it to the list. 
+# When the program completes, sort and print the resulting words in alphabetical order. 
+
+words = []
+try:
+    fhand = open('romeo.txt')
+except:
+    print('File not found')
+    quit()
+for line in fhand:
+    line = line.rstrip()
+    for word in line.split():
+        if word not in words:
+            words.append(word)
+words.sort()
+print(words)
+    
